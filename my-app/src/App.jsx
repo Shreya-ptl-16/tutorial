006557c610caf29078dfa86655b69eff02cf17c4
Shreya-ptl-16 <sp7988252@gmail.com>
2025-03-05 12:11:54 +1030

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,30 @@
-function App(){
-  return<>
-  <h1>Hello</h1>
-  </>
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+
+    </>
+  )
 }
-export default App();
+
+
+export default App;
